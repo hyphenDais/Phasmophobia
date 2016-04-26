98309f6d67f2dfb2f709ae047a4f80bcba7455c2
hyphenDais <nderey6@gmail.com>
2016-04-26 09:51:50 -0600

    Update CollectOrbs.cs

--- Assets/Player Scripts/CollectOrbs.cs
+++ Assets/Player Scripts/CollectOrbs.cs
@@ -6,17 +6,23 @@ public class CollectOrbs : MonoBehaviour {
 
 	public delegate void OrbsCollected();
 	public static event OrbsCollected OnCollect;
+	
+	//List example
 	public List <string> orbList;
 	int i;
 	
 	void OnTriggerEnter(Collider other)
 	{
+		//If statement example
 		if (other.gameObject.tag == "Sphere") 
 		Destroy (other.gameObject);
+		//When the player touches a yellow sphere, it gets destroyed and added to the orbList.
 		orbList.Add (other.gameObject.name);
 		i++;   // every time an integer gets added to the list, i goes up by 1.
 		if (i == 7)
+		{
 		print ("All orbs collected");
+		}
 		if (OnCollect != null)
 			OnCollect();
 	}
